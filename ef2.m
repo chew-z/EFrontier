ad633c63ba5ca0fc019114f0738d1a8449c4f1cc
chew-z <chew-z@outlook.com>
2012-09-07 18:58:31 +0200

    Now we get fully invested and partially invested portfolios

--- ef2.m
+++ ef2.m
@@ -1,20 +1,20 @@
-function [i_tang, sharpe, Sigma, mu] = ef(Mu, Cov, bd, N, rf)
+function [i_tang, sharpe, Sigma, mu] = ef2(Mu, Cov, bd, N, rf)
 % ef - searches for effective frontier in two iterations
 % more smooth around tangency point (max(sharpe))
 % bd - flag [0, 1]. if bd == 1 Sum(w(i)) = 1    
     if nargin < 4, N = 20;          end
     if nargin < 5, rf = 0.00001;    end
 
-    mu = linspace(min(Mu), 5*max(Mu), N);   % How one can find good range (not too wide, not too narrow)? 
+    mu = linspace(min(Mu), 2*max(Mu), N);   % How one can find good range (not too wide, not too narrow)? 
     % Equally spaced linspace here is ineffective, especially with wide range. With narrow range we could not reach max sharpe.
     % For good results we should look in a range [mu(min(sigma)) mu(max(sharpe))] with points concentrated
     % around mu(max(sharpe)) - tangency portfolio.
 
     f = zeros(length(Mu),1);
-    if bd == 1
+    if bd == 1                  % efficient frontier
         Aeq = [ones(1,length(Mu))];
         beq = [1];
-    else
+    else                        % not fully invested. part in tangency portfolio rest in risk-free asset.
         Aeq = [];
         beq = [];
     end 
@@ -29,23 +29,31 @@ function [i_tang, sharpe, Sigma, mu] = ef(Mu, Cov, bd, N, rf)
         end 
     end
     sharpe = (mu-rf) ./ Sigma;
-    imin = find(Sigma == min(Sigma));       % more or less minimu variance point
-    imax = find(sharpe == max(sharpe));     % more or less tangency point
-    % if imin <> imax --> 
-    mu = [linspace(mu(imin), mu(imax-1), N), linspace(mu(imax-1), mu(imax+1), N)];  % overlap in mu(imax-1)
-    w = zeros(length(Mu), 2*N);
-    Sigma = ones(1, 2*N);
+
+        imin = find(Sigma == min(Sigma));       % someplace close to minimum variance point
+        imax = find(sharpe == max(sharpe));     % close to tangency point
+        % if imin <> imax --> 
+        if bd == 1 & imax == N
+            warning('ef2.m: max(sharpe) found at the far end of range [max(Mu)]. Range is probably too narrow and you are not getting near tangency point.');
+            mu = [linspace(mu(imin), mu(imax-1), N), linspace(mu(imax-1), mu(imax), N)];
+        elseif bd == 0
+            mu = linspace(min(Mu), max(Mu), 2*N);
+        else            
+            mu = [linspace(mu(imin), mu(imax-1), N), linspace(mu(imax-1), mu(imax+1), N)];  % overlap in mu(imax-1)
+        end
+        w = zeros(length(Mu), 2*N);
+        Sigma = ones(1, 2*N);
     % Now repeat
-    for i=1: 2*N
-        [x,~,fval] = quadprog(Cov,f,[],[],[Aeq; Mu], [beq; mu(i)],[],[],[],options);
-        if fval > -1, 
-            w(:,i) = x; 
-            Sigma(i) = sqrt(w(:,i)'*Cov*w(:,i));
-        else
-            Sigma(i) = Inf;    % There could be many reasons quadprog returns negative fval. Zero (iterations exceeded maxIter).
-                               % is also not such a great result. Check input. Run with 'Display', 'iter'. 
-        end 
-    end
+        for i=1:2*N
+            [x,~,fval] = quadprog(Cov,f,[],[],[Aeq; Mu], [beq; mu(i)],[],[],[],options);
+            if fval > -1, 
+                w(:,i) = x; 
+                Sigma(i) = sqrt(w(:,i)'*Cov*w(:,i));
+            else
+                Sigma(i) = Inf;    % There could be many reasons quadprog returns negative fval. Zero (iterations exceeded maxIter).
+                                   % is also not such a great result. Check input. Run with 'Display', 'iter'. 
+            end 
+        end
     sharpe = (mu-rf) ./ Sigma;
 
     % sigma_eff = Sigma((mu >= mu(imin)));
--- showEF.m
+++ showEF.m
@@ -3,10 +3,11 @@
 % plots efficient frontier, marks minimum variance point(o) and max sharpe point(x)
 % 
 
-K = 20;			% how many points per each frontier
+K = 10;			% how many points per each frontier
 N = 1;			% how many time steps
-Offset = 400;	% where to start
+Offset = 350;	% where to start
 sharpe= zeros(N, 2*K); sigma = zeros(N, 2*K); mu = zeros(N, 2*K);
+sharpe0= zeros(N, 2*K); sigma0 = zeros(N, 2*K); mu0 = zeros(N, 2*K);
 sh = zeros(N,1); si = zeros(N,1); mi = zeros(N,1);
 sht = zeros(N,1); sit = zeros(N,1); mit = zeros(N,1);
 bounded = 1; % 1 - Sum(weights) = 1
@@ -17,7 +18,8 @@ warning('off', msgid);
 
 for j=Offset+1:Offset+N
 	m = M(j,:); cv = CV(:,:,j);
-	[it, sharpe(j-Offset,:), sigma(j-Offset,:), mu(j-Offset,:)] = ef2(m, cv, bounded, K);						% effective frontier
+	[it, sharpe(j-Offset,:), sigma(j-Offset,:), mu(j-Offset,:)] = ef2(m, cv, 1, K);						% effective frontier
+	[~, sharpe0(j-Offset,:), sigma0(j-Offset,:), mu0(j-Offset,:)] = ef2(m, cv, 0, K);							% combination of tangency portfolio and risk-free asset allocation
 	[sh(j-Offset), si(j-Offset), mi(j-Offset)] = minvar(m, cv);													% minimum variance point
 	if length(it) > 1, it = it(1); end	% give warning
 	sht(j-Offset) = sharpe(j-Offset,it); sit(j-Offset) = sigma(j-Offset,it); mit(j-Offset) = mu(j-Offset,it);	% max sharpe point
@@ -26,19 +28,23 @@ warning('on', msgid);
 
 % plot
 figure(1); clf(1);
-subplot(2,1,1); 		title('Sharpe vs sigma'); xlabel('sigma'); ylabel('sharpe');
+subplot(2,1,1); 		title('Sharpe vs sigma'); xlabel('standard deviation of returns'); ylabel('sharpe ratio');
 for j = 1:N 
-	hold all; plot(sigma(j,:), sharpe(j,:)); 
+	hold all; plot(sigma(j,:), sharpe(j,:));
+	plot(sigma0(j,:), sharpe0(j,:)); 
 	plot(si(j), sh(j),'o-','MarkerSize',10); 
 	plot(sit(j), sht(j),'x-','MarkerSize',10);
 end
-subplot(2,1,2); 		title('Efficient frontier'); xlabel('sigma'); ylabel('returns');
+subplot(2,1,2); 		title('Efficient frontier'); xlabel('standard deviation of returns'); ylabel('expected returns');
 for j = 1:N
-	hold all; plot(sigma(j,:), mu(j,:)); 
+	hold all; plot(sigma(j,:), mu(j,:));
+	plot(sigma0(j,:), mu0(j,:)); 
 	plot(si(j), mi(j), 'o-','MarkerSize',10); 
 	plot(sit(j), mit(j), 'x-','MarkerSize',10);
+	line([0,sit(j)], [0, mit(j)]);
 end
 
 hold off
 
+% clean the mess with variables
 clear m cv j Offset K N msgid bounded
